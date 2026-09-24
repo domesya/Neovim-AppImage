de15f5e622b3497aa310f08beb53647ae9624aef
domesya <163451587+domesya@users.noreply.github.com>
2026-09-24 10:22:38 +0200

    Modify get-dependencies.sh to install more packages
    Updated package installation to include additional dependencies.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm neovim-qt neovim 
+pacman -Syu --noconfirm \
+  qt6ct kvantum lxqt-qtplugin       \
+  neovim-qt neovim 
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano 
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
