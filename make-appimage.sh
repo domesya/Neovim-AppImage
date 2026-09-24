c1803c768763e18139e683ad1e95cec19f30ffbb
domesya <163451587+domesya@users.noreply.github.com>
2026-09-24 10:14:50 +0200

    Modify make-appimage.sh for Neovim configuration
    Updated version retrieval for Neovim and set icon and desktop entry paths.

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q neovim | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/nvim-qt.svg
+export DESKTOP=/usr/share/applications/nvim-qt.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+  /usr/bin/nvim      \
+  /usr/bin/nvim-qt
 
 # Additional changes can be done in between here
 
